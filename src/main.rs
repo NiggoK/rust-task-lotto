cb0e2bbbff7a14156d7e94e3d15cba7d5fcbd617
NicoKatz <62672847+NicoKatz@users.noreply.github.com>
2021-11-23 20:49:45 +0100

    ADD meine Lösung

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::env;
+use std::{env, vec};
 
-use rand::{prelude::IteratorRandom, thread_rng};
+use rand::{/*prelude::IteratorRandom, */ thread_rng, Rng};
 
 struct Lotto {
     take: usize,
@@ -10,22 +10,61 @@ struct Lotto {
 
 impl Lotto {
     fn new(take: usize, from: usize) -> Self {
-        todo!("Implement")
+        // todo!("Implement")
+        let mut rng = thread_rng();
+        let mut numbers: Vec<usize> = vec![];
+        let mut random_number: usize;
+
+        while numbers.len() <= take {
+            random_number = rng.gen_range(1..=from);
+            if !numbers.contains(&random_number) {
+                numbers.push(random_number);
+            }
+        }
+
+        return Self {
+            take,
+            from,
+            numbers,
+        };
     }
 
-    fn get_numbers(self) -> Vec<usize> {
-        todo!("Implement")
+    fn get_numbers(&self) -> Vec<usize> {
+        // self
+        // todo!("Implement")
+        self.numbers.clone()
     }
 }
 
 fn format_lotto_results(lotto: &Lotto) -> String {
     // Tip: Use the format macro
-    todo!("Implement")
+    let msg = format!(
+        "Lotto {} aus {}: {:?}",
+        lotto.take,
+        lotto.from,
+        lotto.get_numbers()
+    );
+    return msg.to_string();
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    todo!("Implement CLI")
+    // TODO soll mehrere auch erlauben %2=0 -> for loop um main()
+    let args_len = args.len();
+    if args_len >= 3 && (args_len % 2) == 1 {
+        for i in (1..=args_len).step_by(2) {
+            // todo!("Implement CLI")
+            let arg1: usize = args[i].parse().expect("Could not parse number of takes");
+            let arg2: usize = args[i + 1]
+                .parse()
+                .expect("Could not parse number of froms");
+            let lotto = Lotto::new(arg1, arg2);
+
+            print!("{} \n", format_lotto_results(&lotto));
+        }
+    } else {
+        print!("Give even amount of numbers");
+    }
 }
 
 #[test]
